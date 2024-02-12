0809ca01ddbd0c1614e900f56d4822fb73925cc2
wreckster1507 <sarthakaitha1507@gmail.com>
2024-02-12 21:10:04 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Passionate about the world of technology and coding.I thrive on the ever-evolving nature of technology and
+        am committed to continuous learning.I explore Full-Stack development, where I enjoy creating seamless,
+        end-to-end solutions. My curiosity drives me to explore new tools, languages, and frameworks. With a
+        growth mindset, I embrace challenges and adapt to emerging trends.Let's connect and discuss how my
+        skills and enthusiasm for technology can contribute to your projects and the broader tech community.
+        I'm always eager to collaborate and learn from like-minded professionals in these exciting fields.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+import {
+    snake,
+    bookstore,
+    campchase,
+    fullstack,
+    unity,
+    foundation,
+    datastructures,
+    csharp,
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    python,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "React Developer",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "AI Developer",
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "figma",
+        icon: figma,
+    },
+    {
+        name: "python",
+        icon: python,
+    },
+    {
+        name: "csharp",
+        icon: csharp
+    },
+];
+
+const experiences = [
+    {
+        title: "Python",
+        company_name: "Python",
+        icon: python,
+        iconBg: "#E6DEDD",
+        date: "july 2022 - sept 2022",
+        points: [
+            "data types ,variables,arrays,tuples,list,set",
+            "decision making statements,loops, functions",
+            "functions,higher order functions",
+            "object oriented programming",
+            "tkinter",
+            "ai and machine learning fundamentals"
+        ],
+    },
+    {
+        title: "Data Structures",
+        company_name: "Python",
+        icon: datastructures,
+        iconBg: "#383E56",
+        date: "sept 2022",
+        points: [
+            "Searching- linear search, binary search",
+            "Sorting- bubbleSort,quickSort,selectionSort,mergeSort",
+            "Stack,Queue,Circular Queue,",
+            "LinkedLists-Singly LinkedList,Doubly LinkedList,Implentation of Stack and Queue using LinkedList",
+            "Tree-Binary Tree, Binary Search Tree,Tree traversal",
+            "Graph-Breadth First Search,Depth First Search"
+        ],
+    },
+    {
+        title: "HTML CSS Javascript",
+        company_name: "Web fundamentals",
+        icon: foundation,
+        iconBg: "#E6DEDD",
+        date: "dec 2022 - march 2023",
+        points: [
+            "HTML",
+            "CSS - Box Model,CSS properties,Responsive CSS,Flexbox,Bootstrap,Bulma",
+            "JavaScript - variables,arrays,objects,decision making statements,loops,functions,callback and arrow functitons",
+            "DOM - eventlisteners,keyboard events,event objects,event bubbling , event delegation",
+        ],
+    },
+    {
+        title: "Unity",
+        company_name: "C#",
+        icon: unity,
+        iconBg: "#383E56",
+        date: "july 2023 - sept 2023",
+        points: [
+            "Learning Unity Interface,Game View,Hierarchy,Inspector",
+            "C# - variables,decision making statements, loops,functions",
+            "Euphoria Engine - Naturalmotion ,unity plugins",
+            "Unity Asset Store",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "html,css,javascript",
+        icon: fullstack,
+        iconBg: "#E6DEDD",
+        date: "sept 2023 - Present",
+        points: [
+            "Asynchronous Javascript (AJAX) , API , node , NPM , creating servers with Express , Dynamic HTML with Templating ",
+            "RESTful Routes , MongoDb , MiddleWare , Error Handling , Data Relationships , Validating Data",
+            "Express Routers , cookies, session , flash , authentication , authorization , deploying",
+            "React - JSX , events , State Concepts , component Design , Effects , Material UI",
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "The Web Developer Bootcamp 2024",
+        name: "Udemy",
+        designation: "ID",
+        company: "UC-a2cd4b8b-c0f2-439c-aad3-40648b56f413",
+        image: "https://d1yei2z3i6k35z.cloudfront.net/161/62e117a8612af_Udemy_Logo.png",
+    },
+    {
+        testimonial:
+            "Python Programming Essentials",
+        name: "Coursera",
+        designation: "ID",
+        company: "ZLBKNFA8MTKP",
+        image: "https://tradesense.ru/img/stocks/COUR.png",
+    },
+    {
+        testimonial:
+            "Python Basics",
+        name: "Coursera",
+        designation: "ID",
+        company: "6XAM6YG5M3VU",
+        image: "https://tradesense.ru/img/stocks/COUR.png",
+    },
+];
+
+const projects = [
+    {
+        name: "CampChase",
+        description:
+            "ChaseCamp is a dynamic web application developed for outdoor enthusiasts who crave unique camping experiences. This platform offers an array of features designed to enhance your journey, from discovering new campgrounds to connecting with a passionate community of fellow campers.",
+        tags: [
+            {
+                name: "HTML,CSS,Javascript",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "node.js,mongoDB,express",
+                color: "green-text-gradient",
+            },
+            {
+                name: "EJS",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: campchase,
+        source_code_link: "https://github.com/wreckster1507/CampChase",
+    },
+    {
+        name: "Bookstore",
+        description:
+            "A full-stack web application for managing a bookstore, built with the MERN stack (MongoDB, Express.js, React, Node.js) and styled with Tailwind CSS.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "node.js,express",
+                color: "green-text-gradient",
+            },
+            {
+                name: "mongoDB",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: bookstore,
+        source_code_link: "https://github.com/wreckster1507/BookStore",
+    },
+    {
+        name: "Snake Game",
+        description:
+            "This is a simple implementation of the classic Snake Game using HTML, CSS, and JavaScript. The game is played on a grid, where the player controls a snake that moves around the board, eats food, and grows longer. The goal is to achieve the highest score by collecting as much food as possible without colliding with the boundaries of the board or the snake itself.",
+        tags: [
+            {
+                name: "HTML",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "CSS",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Javascript",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: snake,
+        source_code_link: "https://github.com/wreckster1507/snake-game",
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
